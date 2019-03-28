01799cecffd9c3946e9c4f20effcc44c17ae120f
uo251119 <uo251119@uniovi.es>
2019-03-28 13:51:33 +0100

    SDI-NodeJS-9.3- Redirección de peticiones

--- app.js
+++ app.js
@@ -94,6 +94,10 @@ app.set('crypto',crypto);
 require("./routes/rusuarios.js")(app, swig, gestorBD);
 require("./routes/rcanciones.js")(app, swig, gestorBD);
 
+app.get('/', function (req, res) {
+    res.redirect('/tienda');
+})
+
 // lanzar el servidor
 app.listen(app.get('port'), function () {
     console.log("Servidor activo");
